6123566f87221ea8284164eb67cdca21b302e248
Zhalgas03 <zhalgas041203@gmail.com>
2026-04-29 20:30:50 +0500

    fix vercel routing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
-import logo from "../assets/logo.png"
+
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false)
